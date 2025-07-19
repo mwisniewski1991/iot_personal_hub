da481310f091a9ef437faa2d7ba7b7612162161c
mwisniewski1991 <mw.data.web@gmail.com>
2025-07-19 23:42:30 +0200

    Add logging

--- api/app.py
+++ api/app.py
@@ -1,12 +1,23 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
+import logging
 
 app = Flask(__name__)
 
 load_dotenv()
-API_KEY = os.getenv('API_KEY')
-print(API_KEY)
+API_KEY = os.getenv('device1_apikey')
+
+# Konfiguracja loggingu
+logging.basicConfig(
+    level=os.getenv('LOG_LEVEL', 'INFO').upper(),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('app.log'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def home():
@@ -24,13 +35,15 @@ def mobile_location():
     try:
         api_key = request.headers.get('X-API-Key')
         if api_key != API_KEY:
+            logger.error(f"Nieprawidłowy klucz API: {api_key}")
             return jsonify({"error": "Nieprawidłowy klucz API"}), 401
         
         data = request.get_json()
-        print(data)
+        logger.info(f"Otrzymano dane: {data}")
         return jsonify({"message": "Lokalizacja odbierana"})
 
     except Exception as e:
+        logger.error(f"Błąd: {str(e)}")
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
