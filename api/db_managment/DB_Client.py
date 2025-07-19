5ca355aa7a7d0e28f0ab70a74d51b909301b822a
mwisniewski1991 <mw.data.web@gmail.com>
2025-07-20 00:19:18 +0200

    Add postgres managment scripts

---
+++ api/db_managment/DB_Client.py
@@ -0,0 +1,40 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+
+class DB_Client:
+    def __init__(self):
+        self.db_client = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+
+    def get_db_client(self):    
+        return self.db_client
+    
+    def close_db_client(self):
+        self.db_client.close()
+
+    def get_cursor(self):
+        return self.db_client.cursor()
+    
+    def close_cursor(self, cursor):
+        cursor.close()
+        
+    def save_data_to_db(self, data: dict):  
+        cursor = self.get_cursor()
+        cursor.execute("INSERT INTO device_locations_test (device_id, latitude, longitude) VALUES (%s, %s, %s)", (data['device_id'], data['latitude'], data['longitude']))
+        self.db_client.commit()
+        self.close_cursor(cursor)
