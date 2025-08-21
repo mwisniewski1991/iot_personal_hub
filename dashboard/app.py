13f3aef5a8ef72476cc5aec84a8bfd8ecfa5c1b0
mwisniewski1991 <mw.data.web@gmail.com>
2025-08-21 10:57:42 +0200

    Add message when data are not available

--- dashboard/app.py
+++ dashboard/app.py
@@ -35,16 +35,21 @@ with col2:
 
 col3, col4 = st.columns(2)
 with col3:
+    pass
     if devices_location_altitude_m:
         df_devices_location_altitude_m = pd.DataFrame(devices_location_altitude_m, columns=['device_timestamp', 'altitude_m'])
         df_devices_location_altitude_m['device_timestamp'] = pd.to_datetime(df_devices_location_altitude_m['device_timestamp'])
         charts.create_chart_devices_location_altitude_m(df_devices_location_altitude_m)
+    else:
+        st.write("Brak danych o wysokości urządzeń")    
 
 with col4:  
     if devices_battery_level:
         df_devices_battery_level = pd.DataFrame(devices_battery_level, columns=['device_timestamp', 'level_min', 'level_avg', 'level_max'])
         df_devices_battery_level['device_timestamp'] = pd.to_datetime(df_devices_battery_level['device_timestamp'])
         charts.create_chart_devices_battery_level(df_devices_battery_level)
+    else:
+        st.write("Brak danych o poziomie baterii urządzeń")
    
 col5, col6 = st.columns(2)
 with col5:
@@ -52,11 +57,13 @@ with col5:
         df_devices_battery_temperature = pd.DataFrame(devices_battery_temperature, columns=['device_timestamp', 'temperature_min', 'temperature_avg', 'temperature_max'])
         df_devices_battery_temperature['device_timestamp'] = pd.to_datetime(df_devices_battery_temperature['device_timestamp'])
         charts.create_chart_devices_battery_temperature(df_devices_battery_temperature)
-
+    else:
+        st.write("Brak danych o temperaturze baterii urządzeń")
 
 with col6:
     if devices_battery_usage_current_mA:
         df_devices_battery_usage_current_mA = pd.DataFrame(devices_battery_usage_current_mA, columns=['device_timestamp', 'usage_current_mA_min', 'usage_current_mA_avg', 'usage_current_mA_max'])
         df_devices_battery_usage_current_mA['device_timestamp'] = pd.to_datetime(df_devices_battery_usage_current_mA['device_timestamp'])
         charts.create_chart_devices_battery_usage_current_mA(df_devices_battery_usage_current_mA)
-
+    else:
+        st.write("Brak danych o zużyciu prądu baterii urządzeń")
